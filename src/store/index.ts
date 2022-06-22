87246d42796c7cbe8ba9625d1fd26788dc0991c8
Kacper <kacperfila26@gmail.com>
2022-06-22 09:43:40 +0200

    Podłączenie wykresów z backiem
    Podłączono wykresy z backiem, dodano przycisk do odświeżania, gdyby wylosowane kolory były zbyt podobne

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,9 @@ export default createStore({
       first_name:'',
       last_name:'',
     },
-    kanbany: []
+    kanbany: [],
+    wykresy: [],
+    wykresy2: [],
   },
   mutations: {
     setUser(state, user){
@@ -28,9 +30,17 @@ export default createStore({
         first_name: '',
         last_name: '',
       };
-      state.kanbany = []
+      state.kanbany = [],
+      state.wykresy = [],
+      state.wykresy2 = []
     },
     setKanbany(state, kanbany) {
+      state.kanbany = kanbany
+    },
+    setWykresy(state, kanbany) {
+      state.kanbany = kanbany
+    },
+    setWykresy2(state, kanbany) {
       state.kanbany = kanbany
     }
   },
@@ -46,12 +56,20 @@ export default createStore({
     },
     getKanbany({ commit }, kanbany) {
       commit('setKanbany', kanbany)
+    },
+    getWykresy({ commit }, wykresy) {
+      commit('setWykresy', wykresy)
+    },
+    getWykresy2({ commit }, wykresy2) {
+      commit('setWykresy2', wykresy2)
     }
   },
   getters: {
     getAuth: (state) => {return state.token != ''},
     getToken: (state) => {return state.token},
     getUser: (state) => { return state.user; },
-    getKanbany: (state) => {return state.kanbany}
+    getKanbany: (state) => {return state.kanbany},
+    getWykresy: (state) => {return state.wykresy},
+    getWykresy2: (state) => {return state.wykresy2},
   }
 })
--- src/views/StatisticsPage.vue
+++ src/views/StatisticsPage.vue
@@ -1,19 +1,8 @@
-<template>
-  <div class="statisticsPage">
-    <Statistics></Statistics>
-  </div>
-</template>
-
-<style lang="scss">
-.statisticsPage {
-  padding-top: 50px;
-}
-</style>
-
 <script lang="ts">
-import Statistics from "../components/Statistics.vue";
 import { defineComponent } from "vue";
-
+import TimeHubClient from "../axios-client";
+import store from "../store";
+import Statistics from "../components/Statistics.vue";
 export default defineComponent({
   components: {
     Statistics,
@@ -27,5 +16,133 @@ export default defineComponent({
       pageName: URLPageName,
     };
   },
+
+  beforeMount: async function () {
+    
+    try {
+      const lewy_wykres_response = await TimeHubClient.get(
+        `statystykaNotatkiSkonczoneAktywne7Dni/`,
+        { headers: { Authorization: `Bearer ${this.token}` } }
+      );
+      const prawy_wykres_response = await TimeHubClient.get(
+        `https://projekt-timehub.herokuapp.com/api/statystykaProcentowaIloscTaskowWTablicach/`,
+        { headers: { Authorization: `Bearer ${this.token}` } }
+      );
+
+      store.dispatch("getWykresy", lewy_wykres_response.data);
+      this.lewy_wykres_label = Object.keys(lewy_wykres_response.data);
+
+      store.dispatch("getWykresy", lewy_wykres_response.data);
+      this.lewy_wykres_data = Object.values(lewy_wykres_response.data);
+
+      store.dispatch("getWykresy", prawy_wykres_response.data);
+      this.prawy_wykres_label = Object.keys(prawy_wykres_response.data);
+
+      store.dispatch("getWykresy", prawy_wykres_response.data);
+      this.prawy_wykres_data = Object.values(prawy_wykres_response.data);
+    } catch (error) {
+      this.lewy_wykres_label = "Nie można było pobrać danych";
+    }
+  },
+  data: () => {
+    return {
+      user: store.getters.getUser,
+      token: store.getters.getToken,
+      lewy_wykres_label: store.getters.getWykresy,
+      lewy_wykres_data: store.getters.getWykresy,
+      prawy_wykres_label: store.getters.getWykresy,
+      prawy_wykres_data: store.getters.getWykresy,
+      lewy_wykres2: store.getters.getWykresy2,
+      tytul: "",
+      czy_zautomatyzowane: false,
+      stat_reload: 0,
+    };
+  },
+  methods: {
+    reload_stat() {
+      
+      console.log("wykonalo sie");
+      this.stat_reload += 1;
+    },
+  },
 });
 </script>
+
+<template>
+  <div class="statisticsPage">
+    <button id="refresh_id" class="refresh_button" v-on:click="reload_stat()">
+      Odśwież wykres
+    </button>
+    <div id="ComponentWrapper">
+      <Statistics
+        :key="stat_reload"
+        :lewy_wykres_label="lewy_wykres_label"
+        :lewy_wykres_data="lewy_wykres_data"
+        :prawy_wykres_label="prawy_wykres_label"
+        :prawy_wykres_data="prawy_wykres_data">
+      </Statistics>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.chartsWrapper {
+  /* glowny container na wykresy */
+  width: 80%;
+  height: 65vh;
+  display: block;
+  margin: 0 auto;
+}
+.leftChartWrapper {
+  /* ustawienia lewego wykresu */
+  width: 50%;
+  height: 40vh;
+  background-color: rgba(255, 255, 255);
+  display: block;
+  float: left;
+  position: relative;
+  padding-bottom: 40px;
+}
+.rightChartWrapper {
+  /* ustawienia prawego wykresu */
+  width: 50%;
+  height: 40vh;
+  background-color: rgba(255, 255, 255);
+  display: block;
+  float: left;
+  position: relative;
+  padding-bottom: 40px;
+}
+.bottomWrapper {
+  /* ustawienia dolnego wykresu */
+  width: 100%;
+  height: 50%;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  display: block;
+  float: left;
+  position: relative;
+}
+h1 {
+  margin: 20px;
+}
+.refresh_button {
+  margin-top: 60px;
+  border: 0;
+  width: 100px;
+  line-height: 1.1;
+  padding: 8px 10px;
+  font-size: 0.8rem;
+  text-align: center;
+  color: #2c3e50;
+  cursor: pointer;
+  border-radius: 16px;
+  background-color: white;
+  box-shadow: 0px 0px 7px gray, 0 0 2px inset gray;
+  enable-background: new 0 0 569.613 569.614;
+}
+.refresh_button:active {
+  box-shadow: 0 0 0, 0 0 5px inset gray;
+  transition-duration: 0.025s;
+}
+</style>
